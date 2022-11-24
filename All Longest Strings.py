8081fd0441d10795832a4b31802acaf2eb5ce20e
Zippo <digeronimovalentin@gmail.com>
2022-11-24 00:12:57 -0300

    Solved -Python

--- All Longest Strings.py
+++ All Longest Strings.py
@@ -9,7 +9,3 @@ def solution(inputArray):
     maxStr = max([len(x) for x in inputArray])
 
     return [x for x in inputArray if (len(x) == maxStr)]
-
-print(solution(["aba", "aa", "ad", "vcd", "aba"]))
-print(solution(["abacaba", "abacab", "abac", "xxxxxx"]))
-print(solution(["enyky", "benyky", "yely", "varennyky"]))
